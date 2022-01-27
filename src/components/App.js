b722d2eba1e0f9f2756d8e66d56a87ccd5014ba2
devmagrfs <devmagrfs@gmail.com>
2022-01-27 17:20:14 +0900

    실제 네트워크 데이터 갖고오기 : fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import ReviewList from './ReviewList';
-import mockitems from '../mock.json';
+import { getReviews } from '../api';
 import { useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState(mockitems);
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState('createdAt');
   // item을 기준에 따라 정렬시키기 위한 코드
   const sortedItems = items.sort((a, b) => b[order] - a[order])
 
   const handleNewestClick = () => setOrder('createdAt');
   const handleBestClick = () => setOrder('rating');
+
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id)
     setItems(nextItems);
+  }
+
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews)
   }
 
   return (
@@ -22,6 +28,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
